7bf8119f8240de52fb4a4157c01382dad47bcde3
shabbir <talkin0x10@gmail.com>
2023-01-17 01:29:53 +0530

    v1

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  password = '';
+  letter: boolean = false;
+  number: boolean = false;
+  symbol: boolean = false;
+
+  length:number=0;
+  onchangeletter() {
+    this.letter = !this.letter;
+  }
+  onchangenumber() {
+    this.number = !this.number;
+  }
+  onchangesymbol() {
+    this.symbol = !this.symbol;
+  }
+  looklength(lvalue: string) {
+    this.length = parseInt(lvalue);
+  }
+onclick(){ let generatedpassword=''
+  let letters="abcdefghijklmnopqrstuvwxyz";
+let numbers="0123456789";
+let symbols="!@#$%^&*()";
+let characters='';
+if(this.letter)
+{
+  characters+=letters;
+
+}
+if(this.number)
+{
+  characters+=numbers;
+  
+}
+if(this.symbol)
+{
+  characters+=symbols;
+  
+}
+for(let i=0;i<this.length;i++){
+  let index=Math.floor(Math.random()*(characters.length));
+generatedpassword+=characters[index];
+}
+this.password=generatedpassword;
+}
+
+}
